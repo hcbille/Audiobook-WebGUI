5ee5150ec84249f10977a71cbacee591aa52d35c
hc <hcbille@gmail.com>
2023-02-12 02:24:23 +0100

    .

--- startup.sh
+++ startup.sh
@@ -2,10 +2,12 @@
 
 FILE=./cred/credentials.json
 if [ -f "$FILE" ]; then
-    node main.js
+    echo "Starting"
 else 
     echo "$FILE does not exist. Made a copy in /cred"
     mkdir ./cred
     cp credentials.json.template ./cred/credentials.json
-    node main.js
+    
 fi
+
+node main.js
